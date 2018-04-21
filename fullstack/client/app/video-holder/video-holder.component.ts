2b84657fb80e00915487807953d67ad0f8a7b074
De Coster Alexandre <a.decoster@overworks.be>
2018-04-21 22:57:55 +0200

    disable video

--- fullstack/client/app/video-holder/video-holder.component.ts
+++ fullstack/client/app/video-holder/video-holder.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, Input, ElementRef, AfterViewInit } from '@angular/core';
-import { easyrtc } from "easyrtc";
+//import { videojs } from 'videojs';
 
 @Component({
-  selector: 'app-video-holder',
+  selector: 'app-video-videojsholder',
   templateUrl: './video-holder.component.html',
   styleUrls: ['./video-holder.component.css']
 })
@@ -16,30 +16,30 @@ export class VideoHolderComponent implements OnInit, AfterViewInit {
   }
   ngAfterViewInit() {
 
-
-    // ID with which to access the template's video element
-    const el = 'video_' + this.idx;
-
-    // setup the player via the unique element ID
-    //this.player = videojs(document.getElementById(el), {}, function () {
-
-    // Store the video object
-    const myPlayer = this, id = myPlayer.id();
-
-    // Make up an aspect ratio
-    const aspectRatio = 264 / 640;
-
-    // internal method to handle a window resize event to adjust the video player
-    function resizeVideoJS() {
-      const width = document.getElementById(id).parentElement.offsetWidth;
-      myPlayer.width(width).height(width * aspectRatio);
-    }
-
-    // Initialize resizeVideoJS()
-    resizeVideoJS();
-
-    // Then on resize call resizeVideoJS()
-    window.onresize = resizeVideoJS;
-  });
-}
+    /* 
+        // ID with which to access the template's video element
+        const el = 'video_' + this.idx;
+    
+        // setup the player via the unique element ID
+        const player = (document.getElementById(el), {}, function () {
+    
+          // Store the video object
+          const myPlayer = this, id = myPlayer.id();
+    
+          // Make up an aspect ratio
+          const aspectRatio = 264 / 640;
+    
+          // internal method to handle a window resize event to adjust the video player
+          function resizeVideoJS() {
+            const width = document.getElementById(id).parentElement.offsetWidth;
+            myPlayer.width(width).height(width * aspectRatio);
+          }
+    
+          // Initialize resizeVideoJS()
+          resizeVideoJS();
+    
+          // Then on resize call resizeVideoJS()
+          window.onresize = resizeVideoJS;
+        }); */
+  }
 }
